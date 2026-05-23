5eb15d81563642a6941b79e7eed36decd7bd6946
Velazquez Ivan <Ivan.VelazquezLeal@alu.frt.utn.edu.ar>
2026-05-22 22:33:55 -0300

    Completado CasoDictionary y EjemploDictionary

--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -21,9 +24,37 @@ internal class Ejemplos
     //Eliminar un alumno por clave y listar por consola los alumnos
     public static void EjemploDictionary()
     {
+        CasoDictionary diccionario = new CasoDictionary();
+        Alumno alumno1 = new Alumno(01, "Juan", 8.5);
+        Alumno alumno2 = new Alumno(02, "Maria", 9.0);
+        Alumno alumno3 = new Alumno(03, "Pedro", 7.5);
 
+        diccionario.AgregarAlumno(alumno1);
+        diccionario.AgregarAlumno(alumno2);
+        diccionario.AgregarAlumno(alumno3);
+
+        ListarDiccionario(diccionario.getDiccionario());
+
+        Console.WriteLine("\nBuscar un alumno por clave y mostrar por consola");
+        Console.WriteLine(diccionario.BuscarAlumno(03));
+
+        Console.WriteLine("\nBuscar un alumno por clave, pero que no exista, y mostrar por consola el texto \"No existe\"");
+        Console.WriteLine(diccionario.BuscarAlumno(05));
+
+        Console.WriteLine("\nEliminar un alumno por clave y listar por consola los alumnos");
+
+        diccionario.EliminarAlumno(02);
+
+        ListarDiccionario(diccionario.getDiccionario());
     }
 
+    public static void ListarDiccionario(Dictionary<int, Alumno> diccionario)
+    {
+        foreach (var alumno in diccionario)
+        {
+            Console.WriteLine(alumno.ToString());
+        }
+    }
     //Realizar una llamada a cada método definido en CasoLinq y mostar por consola según corresponda
     public static void EjemploLinq()
     {
